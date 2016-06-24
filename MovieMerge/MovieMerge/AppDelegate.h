6c4467f6d511a6ad38bdd8e7bd32ed047216cbdc
MacroYang <412041697@qq.com>
2016-06-24 14:10:30 +0800

    frist commit

---
+++ MovieMerge/MovieMerge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieMerge
+//
+//  Created by Macrotellect on 16/6/24.
+//  Copyright © 2016年 Macrotellect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
